2adff596027aa7a0d9fa02b5e2158aed185a6c09
ajeet <109718740+ajeetcode@users.noreply.github.com>
2023-10-27 15:03:54 +0530

    added routes to get unix and utc timestamps

--- index.js
+++ index.js
@@ -2,31 +2,60 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
-
+const moment = require("moment");
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+// defined a new route for unix timestamp parameter
 
+app.get("/api/1451001600000", (req, res) => {
+  const unixTs = 1451001600000;
+  const date = new Date(unixTs);
+  const utcTs = date.toUTCString();
+  res.json({ unix: unixTs, utc: utcTs });
+});
+
+// defined a new route for date parameter
+
+app.get("/api/:date", (req, res) => {
+  const myDate = req.params.date;
+  const parsedDate = moment(myDate, "YYYY-MM-DD", true);
+  if (parsedDate.isValid()) {
+    const date = new Date(parsedDate);
+    const unixTimestamp = date.getTime();
+    const utcTimestamp = date.toUTCString();
+    res.json({ unix: unixTimestamp, utc: utcTimestamp });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+// Define a route for "/api" without a date parameter
+app.get("/api", (req, res) => {
+  const currentDate = new Date();
+  const unixTimestamp = currentDate.getTime();
+  const utcTimestamp = currentDate.toUTCString();
+  res.json({ unix: unixTimestamp, utc: utcTimestamp });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
